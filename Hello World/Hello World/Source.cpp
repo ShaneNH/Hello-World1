27c90e38873ba28ef8c94e7b50c57b20ca669472
Shane Nishioka-Healy <shanenh18@gmail.com>
2014-05-18 16:55:25 -0700

    Hello World Project

---
+++ Hello World/Hello World/Source.cpp
@@ -0,0 +1,14 @@
+///////////////////////////////////
+// Hello World
+// Shane Nishioka-Healy
+// CS-172
+//////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "Hello World" << endl;
+	return 0;
+}
